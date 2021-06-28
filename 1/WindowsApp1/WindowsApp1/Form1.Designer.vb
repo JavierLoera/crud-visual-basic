671c3d17d73c007ba2f41014e4352ac9fb320a81
Jesus Loera <jesus_loera_15@hotmail.com>
2021-06-28 10:07:17 -0500

    primer cambio

---
+++ 1/WindowsApp1/WindowsApp1/Form1.Designer.vb
@@ -0,0 +1,383 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtnoempleado = New System.Windows.Forms.TextBox()
+        Me.txtnombre = New System.Windows.Forms.TextBox()
+        Me.txtappat = New System.Windows.Forms.TextBox()
+        Me.txtdepto = New System.Windows.Forms.TextBox()
+        Me.txtapmat = New System.Windows.Forms.TextBox()
+        Me.txtbuscar = New System.Windows.Forms.TextBox()
+        Me.Txtfechanacimiento = New System.Windows.Forms.TextBox()
+        Me.txtdireccion = New System.Windows.Forms.TextBox()
+        Me.Txtsueldo = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.btnMostrar = New System.Windows.Forms.Button()
+        Me.btnNuevo = New System.Windows.Forms.Button()
+        Me.btnSalir = New System.Windows.Forms.Button()
+        Me.btnEliminar = New System.Windows.Forms.Button()
+        Me.btnModificar = New System.Windows.Forms.Button()
+        Me.btnGuardar = New System.Windows.Forms.Button()
+        Me.btnBuscar = New System.Windows.Forms.Button()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'txtnoempleado
+        '
+        Me.txtnoempleado.Location = New System.Drawing.Point(212, 41)
+        Me.txtnoempleado.Name = "txtnoempleado"
+        Me.txtnoempleado.Size = New System.Drawing.Size(400, 20)
+        Me.txtnoempleado.TabIndex = 0
+        '
+        'txtnombre
+        '
+        Me.txtnombre.Location = New System.Drawing.Point(212, 73)
+        Me.txtnombre.Name = "txtnombre"
+        Me.txtnombre.Size = New System.Drawing.Size(400, 20)
+        Me.txtnombre.TabIndex = 1
+        '
+        'txtappat
+        '
+        Me.txtappat.Location = New System.Drawing.Point(212, 107)
+        Me.txtappat.Name = "txtappat"
+        Me.txtappat.Size = New System.Drawing.Size(400, 20)
+        Me.txtappat.TabIndex = 2
+        '
+        'txtdepto
+        '
+        Me.txtdepto.Location = New System.Drawing.Point(212, 241)
+        Me.txtdepto.Name = "txtdepto"
+        Me.txtdepto.Size = New System.Drawing.Size(400, 20)
+        Me.txtdepto.TabIndex = 3
+        '
+        'txtapmat
+        '
+        Me.txtapmat.Location = New System.Drawing.Point(212, 141)
+        Me.txtapmat.Name = "txtapmat"
+        Me.txtapmat.Size = New System.Drawing.Size(400, 20)
+        Me.txtapmat.TabIndex = 4
+        '
+        'txtbuscar
+        '
+        Me.txtbuscar.Location = New System.Drawing.Point(212, 312)
+        Me.txtbuscar.Name = "txtbuscar"
+        Me.txtbuscar.Size = New System.Drawing.Size(400, 20)
+        Me.txtbuscar.TabIndex = 5
+        '
+        'Txtfechanacimiento
+        '
+        Me.Txtfechanacimiento.Location = New System.Drawing.Point(212, 174)
+        Me.Txtfechanacimiento.Name = "Txtfechanacimiento"
+        Me.Txtfechanacimiento.Size = New System.Drawing.Size(400, 20)
+        Me.Txtfechanacimiento.TabIndex = 6
+        '
+        'txtdireccion
+        '
+        Me.txtdireccion.Location = New System.Drawing.Point(212, 208)
+        Me.txtdireccion.Name = "txtdireccion"
+        Me.txtdireccion.Size = New System.Drawing.Size(400, 20)
+        Me.txtdireccion.TabIndex = 7
+        '
+        'Txtsueldo
+        '
+        Me.Txtsueldo.Location = New System.Drawing.Point(212, 277)
+        Me.Txtsueldo.Name = "Txtsueldo"
+        Me.Txtsueldo.Size = New System.Drawing.Size(400, 20)
+        Me.Txtsueldo.TabIndex = 8
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.SystemColors.InactiveCaption
+        Me.Label1.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.Label1.Location = New System.Drawing.Point(106, 44)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(71, 13)
+        Me.Label1.TabIndex = 9
+        Me.Label1.Text = "No Empleado"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.SystemColors.InactiveCaption
+        Me.Label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Label2.Location = New System.Drawing.Point(106, 80)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(44, 13)
+        Me.Label2.TabIndex = 10
+        Me.Label2.Text = "Nombre"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.BackColor = System.Drawing.SystemColors.InactiveCaption
+        Me.Label3.Location = New System.Drawing.Point(106, 114)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(84, 13)
+        Me.Label3.TabIndex = 11
+        Me.Label3.Text = "Apellido Paterno"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.BackColor = System.Drawing.SystemColors.InactiveCaption
+        Me.Label4.Location = New System.Drawing.Point(106, 148)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(86, 13)
+        Me.Label4.TabIndex = 12
+        Me.Label4.Text = "Apellido Materno"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.BackColor = System.Drawing.SystemColors.InactiveCaption
+        Me.Label5.Location = New System.Drawing.Point(106, 181)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(93, 13)
+        Me.Label5.TabIndex = 13
+        Me.Label5.Text = "Fecha Nacimiento"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.BackColor = System.Drawing.SystemColors.InactiveCaption
+        Me.Label6.Location = New System.Drawing.Point(106, 215)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(52, 13)
+        Me.Label6.TabIndex = 14
+        Me.Label6.Text = "Direccion"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.BackColor = System.Drawing.SystemColors.InactiveCaption
+        Me.Label7.Location = New System.Drawing.Point(106, 248)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(74, 13)
+        Me.Label7.TabIndex = 15
+        Me.Label7.Text = "Departamento"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.BackColor = System.Drawing.SystemColors.InactiveCaption
+        Me.Label8.Location = New System.Drawing.Point(106, 284)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(40, 13)
+        Me.Label8.TabIndex = 16
+        Me.Label8.Text = "Sueldo"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.BackColor = System.Drawing.SystemColors.InactiveCaption
+        Me.Label9.Location = New System.Drawing.Point(106, 319)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(40, 13)
+        Me.Label9.TabIndex = 17
+        Me.Label9.Text = "Buscar"
+        '
+        'btnMostrar
+        '
+        Me.btnMostrar.BackColor = System.Drawing.SystemColors.Highlight
+        Me.btnMostrar.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.btnMostrar.Location = New System.Drawing.Point(677, 34)
+        Me.btnMostrar.Name = "btnMostrar"
+        Me.btnMostrar.Size = New System.Drawing.Size(75, 23)
+        Me.btnMostrar.TabIndex = 18
+        Me.btnMostrar.Text = "Mostrar"
+        Me.btnMostrar.UseVisualStyleBackColor = False
+        '
+        'btnNuevo
+        '
+        Me.btnNuevo.BackColor = System.Drawing.SystemColors.Highlight
+        Me.btnNuevo.Location = New System.Drawing.Point(677, 63)
+        Me.btnNuevo.Name = "btnNuevo"
+        Me.btnNuevo.Size = New System.Drawing.Size(75, 23)
+        Me.btnNuevo.TabIndex = 19
+        Me.btnNuevo.Text = "Nuevo"
+        Me.btnNuevo.UseVisualStyleBackColor = False
+        '
+        'btnSalir
+        '
+        Me.btnSalir.BackColor = System.Drawing.SystemColors.ControlText
+        Me.btnSalir.ForeColor = System.Drawing.SystemColors.Control
+        Me.btnSalir.Location = New System.Drawing.Point(677, 143)
+        Me.btnSalir.Name = "btnSalir"
+        Me.btnSalir.Size = New System.Drawing.Size(75, 23)
+        Me.btnSalir.TabIndex = 20
+        Me.btnSalir.Text = "salir"
+        Me.btnSalir.UseVisualStyleBackColor = False
+        '
+        'btnEliminar
+        '
+        Me.btnEliminar.BackColor = System.Drawing.SystemColors.ControlLight
+        Me.btnEliminar.Location = New System.Drawing.Point(257, 525)
+        Me.btnEliminar.Name = "btnEliminar"
+        Me.btnEliminar.Size = New System.Drawing.Size(75, 23)
+        Me.btnEliminar.TabIndex = 21
+        Me.btnEliminar.Text = "Eliminar"
+        Me.btnEliminar.UseVisualStyleBackColor = False
+        '
+        'btnModificar
+        '
+        Me.btnModificar.Location = New System.Drawing.Point(163, 525)
+        Me.btnModificar.Name = "btnModificar"
+        Me.btnModificar.Size = New System.Drawing.Size(75, 23)
+        Me.btnModificar.TabIndex = 22
+        Me.btnModificar.Text = "Modificar"
+        Me.btnModificar.UseVisualStyleBackColor = True
+        '
+        'btnGuardar
+        '
+        Me.btnGuardar.Location = New System.Drawing.Point(82, 525)
+        Me.btnGuardar.Name = "btnGuardar"
+        Me.btnGuardar.Size = New System.Drawing.Size(75, 23)
+        Me.btnGuardar.TabIndex = 23
+        Me.btnGuardar.Text = "Guardar"
+        Me.btnGuardar.UseVisualStyleBackColor = True
+        '
+        'btnBuscar
+        '
+        Me.btnBuscar.Location = New System.Drawing.Point(1, 525)
+        Me.btnBuscar.Name = "btnBuscar"
+        Me.btnBuscar.Size = New System.Drawing.Size(75, 23)
+        Me.btnBuscar.TabIndex = 24
+        Me.btnBuscar.Text = "Buscar"
+        Me.btnBuscar.UseVisualStyleBackColor = True
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Location = New System.Drawing.Point(677, 241)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(72, 17)
+        Me.RadioButton1.TabIndex = 25
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "Empleado"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Location = New System.Drawing.Point(677, 277)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(92, 17)
+        Me.RadioButton2.TabIndex = 26
+        Me.RadioButton2.TabStop = True
+        Me.RadioButton2.Text = "Departamento"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.BackgroundColor = System.Drawing.SystemColors.ControlLight
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(109, 349)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(582, 170)
+        Me.DataGridView1.TabIndex = 27
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.HotTrack
+        Me.ClientSize = New System.Drawing.Size(800, 574)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.RadioButton2)
+        Me.Controls.Add(Me.RadioButton1)
+        Me.Controls.Add(Me.btnBuscar)
+        Me.Controls.Add(Me.btnGuardar)
+        Me.Controls.Add(Me.btnModificar)
+        Me.Controls.Add(Me.btnEliminar)
+        Me.Controls.Add(Me.btnSalir)
+        Me.Controls.Add(Me.btnNuevo)
+        Me.Controls.Add(Me.btnMostrar)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Txtsueldo)
+        Me.Controls.Add(Me.txtdireccion)
+        Me.Controls.Add(Me.Txtfechanacimiento)
+        Me.Controls.Add(Me.txtbuscar)
+        Me.Controls.Add(Me.txtapmat)
+        Me.Controls.Add(Me.txtdepto)
+        Me.Controls.Add(Me.txtappat)
+        Me.Controls.Add(Me.txtnombre)
+        Me.Controls.Add(Me.txtnoempleado)
+        Me.Controls.Add(Me.Label1)
+        Me.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtnoempleado As TextBox
+    Friend WithEvents txtnombre As TextBox
+    Friend WithEvents txtappat As TextBox
+    Friend WithEvents txtdepto As TextBox
+    Friend WithEvents txtapmat As TextBox
+    Friend WithEvents txtbuscar As TextBox
+    Friend WithEvents Txtfechanacimiento As TextBox
+    Friend WithEvents txtdireccion As TextBox
+    Friend WithEvents Txtsueldo As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents btnMostrar As Button
+    Friend WithEvents btnNuevo As Button
+    Friend WithEvents btnSalir As Button
+    Friend WithEvents btnEliminar As Button
+    Friend WithEvents btnModificar As Button
+    Friend WithEvents btnGuardar As Button
+    Friend WithEvents btnBuscar As Button
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents DataGridView1 As DataGridView
+End Class
---
+++ 1/WindowsApp1/WindowsApp1/Form1.vb
@@ -0,0 +1,107 @@
+﻿Imports System.Data.SqlClient
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Call mostrar empleado()
+    End Sub
+
+    Private Sub BtnNuevo_Click(sender As Object, e As EventArgs) Handles btnNuevo.Click
+        Call limpiar()
+    End Sub
+
+    Sub limpiar()
+        txtnoempleado.Clear()
+        txtnombre.Clear()
+        Txtfechanacimiento.Clear()
+        txtapmat.Clear()
+        txtappat.Clear()
+        txtdireccion.Clear()
+        txtdepto.Clear()
+        Txtsueldo.Clear()
+        txtbuscar.Clear()
+
+    End Sub
+
+    Private Sub BtnGuardar_Click(sender As Object, e As EventArgs) Handles btnGuardar.Click
+        conexion.open()
+        Dim CMD. As New sqlcommand("guardar",conexion)
+            CMD.commandType = CommandType.StoredProcedure
+        whit CMD
+            .parameters.Addwhitvalue
+        ("@No_empleado", Me.txtempleado.text)
+        ("@Ap_paterno", Me.txtappaterno.text)
+        ("@Ap_Materno", Me.txtapmaterno.text)
+        ("@Nombre", Me.txtnombre.text)
+        ("@fecha_nacimiento", Me.txtfechanac.text)
+        ("@Direccion", Me.txtdireccion.text)
+        ("@depto", Me.txtdepartamento.text)
+        ("@Sueldo", Me.txtsueldo.text)
+        .ExecuteNonQuery()
+
+        End Whit
+            MsgBox("empleado agregado", MsgBoxStyle.Information + MsgBoxStyle.OkOnly, "agregar")
+        Call btnMostrarempleado()
+        Call limpiar()
+        txtnoempleado.Focus()
+        conexion.close()
+
+    End Sub
+
+    Private Sub BtnMostrar_Click(sender As Object, e As EventArgs) Handles btnMostrar.Click
+        Call mostrar Empleado()
+    End Sub
+
+    Private Sub BtnSalir_Click(sender As Object, e As EventArgs) Handles btnSalir.Click
+        End
+    End Sub
+
+    Private Sub DataGridView1_Click(sender As Object, e As EventArgs) Handles DataGridView1.Click
+        Dim Fila As DataGrindviewRow = DataGrindview1 current Row
+        txtnoempleado.Text = CStr(Fila.cells(0).value)
+        txtappat.Text = (Fila.cells(1).value)
+        txtapmat.Text = (Fila.cells(2).value)
+        txtnombre.Text = (Fila.cells(3).value)
+        Txtfechanacimiento.Text = (Str(Fila.cells(4).value))
+        txtdireccion.Text = (Fila.cells(5).value)
+        txtdepto.Text = (Fila.cells(6).value)
+        Txtsueldo.Text = (Str(Fila.cells(7).value))
+    End Sub
+
+    Private Sub BtnBuscar_Click(sender As Object, e As EventArgs) Handles btnBuscar.Click
+        If txtbuscar = "" Then
+            MsgBox("Debe escribir algo en el campo", MsgBoxStyle.Information + MsgBoxStyle.OkOnly, "Busqueda")
+
+        ElseIf rbtnoempleado.checked Then
+            txtbuscar.Focus()
+            Dim Da As New SqlDataAdapter(consulta_empleado, conexion)
+            Da.Selectedcommand.commandType = CommandType.StoredProcedure
+            Da.selectedcommand Pameters.Addwhitvalue("@Variable no_empleado", Me.txtbuscar.Text)
+
+Dim DT As New DataTable
+            Da.Fill(DT)
+            Me.DataGrindView1.Datasource = DT
+            Call limpiar()
+
+        ElseIf rbtnoempleado.checked Then
+            txtbuscar.Focus()
+            Da.selectedcommand.parameters.Addwhitvalue
+("@variable departamento", Me.txtbuscar.text)
+
+Else
+            MsgBox("Debe seleccionar una opcion de busqueda", MsgBoxStyle.Information + MsgBoxStyle.Ok only, "Buscar")
+            txtbuscar.Focus()
+            Call limpiar()
+        End If
+    End Sub
+End Class
+
+Public Sub Mostrar empleado()
+    Dim DA As New SqlDataAdapter("consulta", conexion)
+    Dim DS As New DataSet
+    DA fill(DS, "Empleado")
+        DataGrindView.Datasource = DS.Tables("Empleado")
+End Sub
+
+Private Function conexion() As String
+    Throw New NotImplementedException()
+End Function
